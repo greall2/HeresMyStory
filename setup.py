9086e401df1e0f850782745d0d0cc1d908aacdfd
rionagreally <rionagreally@gmail.com>
2016-12-01 12:52:06 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,11 @@ db = couch.create('stories')
 #formatting Date and Time 
 currDate = time.strftime("%c")
 
-#creating Document in stories
-doc ={'story': 'This is my Story', 'Date': currDate}
+#creating Documents in stories
+doc ={'story': 'This is my First Story', 'Date': currDate}
+doc1 ={'story': 'Three Irishmen, Paddy, Sean and Seamus, were stumbling home from the pub late one night and found themselves on the road which led past the old graveyard."Come have a look over here," says Paddy, "Its Michael O Gradys grave, God bless his soul. He lived to the ripe old age of 87." "That is nothing," says Sean, "here is one named Patrick O Toole, it says here that he was 95 when he died." Just then, Seamus yells out, "Good God, heres a fella that got to be 145!" "What was his name?" asks Paddy. Seamus stumbles around a bit, awkwardly lights a match to see what else is written on the stone marker and exclaims: "Miles, from Dublin." - Timmy K, Kerry', 'Date': currDate}
 
-#saving the document to database
+
+#saving the documents to database
 db.save(doc)
+db.save(doc1)
